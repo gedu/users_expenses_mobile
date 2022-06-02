ee84ecec1ddc4b8bedcdea75ba95bae2c0a68afc
Eduardo Graciano <>
2022-06-02 08:19:06 -0300

    navigation from splash to home, base type for navigation

--- usersExpenses/src/routes/routes.tsx
+++ usersExpenses/src/routes/routes.tsx
@@ -1,18 +1,37 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  NativeStackScreenProps,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
 import React from 'react';
 import { HomePage } from '../home/HomePage';
 import { SplashPage } from '../splash/SplashPage';
 
-const Stack = createNativeStackNavigator();
-
 export const RouteName = {
   splash: 'Splash',
   home: 'Home',
+} as const;
+
+export type StackParams = {
+  [RouteName.splash]: undefined;
+  [RouteName.home]: undefined;
+};
+
+export type NavigationProps = {
+  [RouteName.splash]: NativeStackScreenProps<
+    StackParams,
+    'Splash'
+  >['navigation'];
+  [RouteName.home]: NativeStackScreenProps<StackParams, 'Home'>['navigation'];
+};
+
+const Stack = createNativeStackNavigator<StackParams>();
+const mainOptions = {
+  headerShown: false,
 };
 
 export const ExpensesRoutes = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={mainOptions}>
       <Stack.Screen name={RouteName.splash} component={SplashPage} />
       <Stack.Screen name={RouteName.home} component={HomePage} />
     </Stack.Navigator>
--- usersExpenses/src/splash/SplashPage.tsx
+++ usersExpenses/src/splash/SplashPage.tsx
@@ -1,9 +1,24 @@
 import LottieView from 'lottie-react-native';
 import React from 'react';
+import { useEffect } from 'react';
 import { Text, View } from 'react-native';
+import { NavigationProps } from '../routes';
 import { styles } from './SplashPage.style';
 
-export const SplashPage = () => {
+type SplashPageProps = {
+  navigation: NavigationProps['Splash'];
+};
+
+export const SplashPage = ({ navigation }: SplashPageProps) => {
+  useEffect(() => {
+    const cancelId = setTimeout(() => {
+      navigation.navigate('Home');
+    }, 3000);
+    return () => {
+      clearTimeout(cancelId);
+    };
+  }, [navigation]);
+
   return (
     <View style={styles.root}>
       <Text style={styles.title}>Expenses</Text>
