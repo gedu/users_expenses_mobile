9bccf1d9e75a885763357a8df3a867f03b575db0
Eduardo Graciano <>
2022-06-18 12:06:20 +0200

    base config so the app render test pass

--- usersExpenses/__tests__/App-test.tsx
+++ usersExpenses/__tests__/App-test.tsx
@@ -1,14 +1,40 @@
-/**
- * @format
- */
-
 import 'react-native';
+import { render } from '@testing-library/react-native';
 import React from 'react';
-import renderer from 'react-test-renderer';
+
 import App from '../src/App';
 
-// Note: test renderer must be required after react-native.
+jest.mock('react-native-toast-message', () => {
+  const RealComponent = jest.requireActual('react-native-toast-message');
+  const ReactInternal = require('react');
+  class Toast extends ReactInternal.Component {
+    static show = jest.fn();
+    static hide = jest.fn();
+
+    render() {
+      return ReactInternal.createElement(
+        'Toast',
+        this.props,
+        this.props.children,
+      );
+    }
+  }
+  Toast.propTypes = RealComponent.propTypes;
+  return Toast;
+});
+
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+}));
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  render(<App />);
 });
